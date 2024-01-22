a2fdba143f7c68ee09236c69bf27abcd0fbe4062
kmj <119595705+skwnddp@users.noreply.github.com>
2024-01-22 22:56:45 +0900

    Create python_cute.py

---
+++ python_cute.py
@@ -0,0 +1,15 @@
+def print_heart():
+    heart = [
+        "  ♥   ♥  ",
+        " ♥  ♥  ♥ ",
+        "♥      ♥",
+        " ♥    ♥ ",
+        "  ♥  ♥  ",
+        "    ♥    "
+    ]
+
+    for line in heart:
+        print(line)
+
+# Calling the function to print the heart
+print_heart()
